2ca6628cbd4f4b500b5bb4e885c28780f7320264
gundorit <packdev937@gmail.com>
2023-07-11 21:14:53 +0900

    feat : @ResponseBody로 에러 메세지를 JSON 형태로 반환

--- src/main/java/com/example/askme/controller/ExceptionController.java
+++ src/main/java/com/example/askme/controller/ExceptionController.java
@@ -1,18 +1,31 @@
 package com.example.askme.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionController {
 
+    // MethodArgumentNotValidException가 발생했을 때만 해당 메소드가 실행
+    // 처음에는 test가 실행이 안된다. 왜냐하면 JSON 형태로 반환해야 되는데 @RestController가 아닌  @ControllerAdvice 이기 때문에
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(Exception.class)
-    public void exceptionHandler(Exception e) {
-        log.info("exceptionHandler {}", e);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public @ResponseBody Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String message = fieldError.getDefaultMessage();
+
+        Map<String, String> response = new HashMap<>();
+        response.put(field, message);
+        return response;
     }
 }
