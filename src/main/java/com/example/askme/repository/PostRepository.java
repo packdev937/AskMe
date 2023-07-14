c051738e63c494979f3595c18159c70ea19ae856
gundorit <packdev937@gmail.com>
2023-07-14 15:54:00 +0900

    feat : PostRepository 생성

---
+++ src/main/java/com/example/askme/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.example.askme.repository;
+
+import com.example.askme.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
