565789c3c9fc37e11ec9b5d3c2da472a57243cac
gundorit <packdev937@gmail.com>
2023-07-14 15:52:37 +0900

    feat : Post Entity 생성

---
+++ src/main/java/com/example/askme/domain/Post.java
@@ -0,0 +1,31 @@
+package com.example.askme.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob // @Lob이 뭔가? DB에서는 Long Text의 형태로 생성되도록
+    private String content;
+}
