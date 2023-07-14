12e2ea0eaede37ff0036e0def2fb917d05829458
gundorit <packdev937@gmail.com>
2023-07-14 15:53:47 +0900

    feat : Repository 의존성 주입 후 저장 로직 생성

--- src/main/java/com/example/askme/service/PostService.java
+++ src/main/java/com/example/askme/service/PostService.java
@@ -1,8 +1,21 @@
 package com.example.askme.service;
 
+import com.example.askme.domain.Post;
+import com.example.askme.repository.PostRepository;
+import com.example.askme.request.PostCreateDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class PostService {
 
+    private final PostRepository postRepository;
+
+    public void write(PostCreateDto postCreateDto) {
+        Post post = new Post(postCreateDto.getTitle(), postCreateDto.getContent());
+        postRepository.save(post);
+    }
 }
