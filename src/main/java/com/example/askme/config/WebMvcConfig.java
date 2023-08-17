ed1bb289a149d35586c479d8fb405755786f2f6f
gundorit <packdev937@gmail.com>
2023-08-18 04:12:54 +0900

    add: WebMvcConfig.java

---
+++ src/main/java/com/example/askme/config/WebMvcConfig.java
@@ -0,0 +1,12 @@
+package com.example.askme.config;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor());
+    }
+}
