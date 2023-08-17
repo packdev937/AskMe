d8f1cb38a7115a372f65b39690a584024e696c72
gundorit <packdev937@gmail.com>
2023-08-18 04:18:29 +0900

    feat : 401 상태 코드를 반환하도록 설정

--- src/main/java/com/example/askme/exception/UnauthorizedException.java
+++ src/main/java/com/example/askme/exception/UnauthorizedException.java
@@ -1,5 +1,13 @@
 package com.example.askme.exception;
 
-public class UnauthorizedException {
+public class UnauthorizedException extends RuntimeException{
+    private static final String MESSAGE = "인증이 필요합니다";
 
+    public UnauthorizedException() {
+        super(MESSAGE);
+    }
+
+    public int getStatusCode(){
+        return 401;
+    }
 }
